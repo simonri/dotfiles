13ecb5002c853c8102ebc75d509db74e181bec63
Simon Risberg <simon@risberg.eu>
2020-10-08 09:51:37 +0200

    d

--- utils/install.bat
+++ utils/install.bat
@@ -1,10 +1,8 @@
 @ECHO OFF
 
-SET NVIM_PATH=%LocalAppData%\nvim
+SET APPDATA_PATH=%LocalAppData%\nvim
 SET COC_PATH=%UserProfile%\.config\coc
-SET CONFIG_PATH=%UserProfile%\.config\nvim
-
-ECHO %NVIM_PATH%
+SET NVIM_PATH=%UserProfile%\.config\nvim
 
 echo Installing Neovim config...
 
@@ -27,40 +25,41 @@ ECHO https://github.com/universal-ctags/ctags-win32/ && GOTO :EOF
 
 :MOVE_OLD_NVIM_DIR
 IF EXIST %NVIM_PATH% (
-  :: RENAME "%NVIM_PATH%" "nvim.old"
   RMDIR /Q/S "%NVIM_PATH%"
 )
 MKDIR "%NVIM_PATH%"
 
 IF EXIST %CONFIG_PATH% (
-  :: RENAME "%NVIM_PATH%" "nvim.old"
   RMDIR /Q/S "%CONFIG_PATH%"
-  MKDIR "%CONFIG_PATH%"
 )
 MKDIR "%CONFIG_PATH%"
 
 :MOVE_OLD_COC_DIR
 IF EXIST %COC_PATH% (
-  :: RENAME %CONFIG_PATH%/../coc "coc.old"
   RMDIR /Q/S "%COC_PATH%"
-  MKDIR "%COC_PATH%"
 )
 MKDIR "%COC_PATH%"
 
 :CLONE_CONFIG
 ECHO Cloning configuration
-git clone https://github.com/simonri/nvim-config.git %CONFIG_PATH%
+git clone https://github.com/simonri/nvim-config.git %NVIM_PATH%
 
 :INSTALL_PLUGINS
 WHERE nvim 2>NUL >NUL || GOTO :EOF
 
-MOVE "%CONFIG_PATH%\autoload" "%NVIM_PATH%"
-REN "%CONFIG_PATH%\init.vim" "init.vim.tmp" && MOVE "%CONFIG_PATH%\init.vim.tmp" "%NVIM_PATH%"
-MOVE "%CONFIG_PATH%\utils\init.vim" "%NVIM_PATH%"
-echo Installing plugins
+ECHO Add fake init file
+MOVE "%NVIM_PATH%\appdata.init.vim" "%APPDATA_PATH%\"
+REN "%APPDATA_PATH%\appdata.init.vim" "init.vim"
+
+ECHO Move tmp init to NVIM_PATH
+REN "%NVIM_PATH%\init.vim" "init.vim.tmp"
+MOVE "%NVIM_PATH%\utils\init.vim" "%NVIM_PATH%"
+
+ECHO Installing plugins
 nvim --headless +PlugInstall +qall
-MOVE "%NVIM_PATH%\init.vim" "%CONFIG_PATH%\utils"
+
+MOVE "%NVIM_PATH%\init.vim" "%NVIM_PATH%\utils"
 REN "%NVIM_PATH%\init.vim.tmp" "init.vim"
 
 :INSTALL_COC_EXTENSIONS
-MKDIR "%CONFIG_PATH%/..coc/extensions"
+MKDIR "%NVIM_PATH%/../coc/extensions"
